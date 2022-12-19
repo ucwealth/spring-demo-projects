4ad2dc211aa22f7f9178d6c8b4934bc54e3a1c2e
ucwealth <ucheokeke16@gmail.com>
2022-12-19 17:37:53 +0100

    implement update students

--- student-management-system/src/main/java/com/ucwealth/sms/controller/StudentController.java
+++ student-management-system/src/main/java/com/ucwealth/sms/controller/StudentController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.ucwealth.sms.entity.Student;
 import com.ucwealth.sms.service.StudentService;
@@ -35,6 +37,25 @@ public class StudentController {
 	@PostMapping("/students")
 	public String saveStudent(@ModelAttribute("student") Student student) {
 		studentService.saveStudent(student);
+		return "redirect:/students";
+	}
+	
+	@GetMapping("/students/edit/{id}")
+	public String editStudentForm(@PathVariable Long id, Model model) {
+		model.addAttribute("student", studentService.getStudentById(id));
+		return "edit_student";
+	}
+	
+	@PostMapping("/students/{id}")
+	public String updateStudent(@ModelAttribute("student") Student student, @PathVariable Long id, Model model) {
+		// Get Student from db by id
+		Student existingStudent = studentService.getStudentById(id);
+		existingStudent.setId(id);
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		existingStudent.setEmail(student.getEmail());
+		// save updated student object
+		studentService.updateStudent(existingStudent);
 		return "redirect:/students";
 	}
 	
