7f96ea8cec477b834dcdd335948adea60affbf68
ucwealth <ucheokeke16@gmail.com>
2022-12-22 11:03:10 +0100

    make delete work

--- student-management-system/src/main/java/com/ucwealth/sms/controller/StudentController.java
+++ student-management-system/src/main/java/com/ucwealth/sms/controller/StudentController.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import com.ucwealth.sms.entity.Student;
 import com.ucwealth.sms.service.StudentService;
@@ -60,7 +58,7 @@ public class StudentController {
 		return "redirect:/students";
 	}
 	
-	@DeleteMapping("/students/{id}")
+	@GetMapping("/students/{id}")
 	public String deleteStudent(@ModelAttribute("student") Student student, @PathVariable Long id, Model model){
 		studentService.deleteStudentById(id);
 		return "redirect:/students";
