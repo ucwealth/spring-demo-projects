b6d894753803781b8dc50b644769acc19bcb716c
ucwealth <ucheokeke16@gmail.com>
2022-12-22 17:44:22 +0100

    create employee ist component

---
+++ employee-manager-frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent {
+  employees?: Employee[];
+}
