9001e8b6beb23cac5322925a8f3ff1af0f7a89f8
ucwealth <ucheokeke16@gmail.com>
2022-12-22 18:32:21 +0100

    fix cors error

--- employee-manager-backend/src/main/java/com/ucwealth/employee_manager/controller/EmployeeController.java
+++ employee-manager-backend/src/main/java/com/ucwealth/employee_manager/controller/EmployeeController.java
@@ -3,6 +3,7 @@
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ucwealth.employee_manager.model.Employee;
 import com.ucwealth.employee_manager.repository.EmployeeRepository;
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 	@Autowired
