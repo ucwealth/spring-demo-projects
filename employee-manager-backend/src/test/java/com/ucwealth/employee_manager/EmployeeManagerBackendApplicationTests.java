242ee57873822e6e05b459181282be4a9b0d10fa
ucwealth <ucheokeke16@gmail.com>
2022-12-22 11:54:42 +0100

    create employee manager

---
+++ employee-manager-backend/src/test/java/com/ucwealth/employee_manager/EmployeeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucwealth.employee_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
