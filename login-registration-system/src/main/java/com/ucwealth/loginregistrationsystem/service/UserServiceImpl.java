d438124123624ca5aa130863fec02798e0f0f5ea
ucwealth <ucheokeke16@gmail.com>
2022-12-28 14:31:17 +0100

    implement user service

---
+++ login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ucwealth.loginregistrationsystem.service;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import com.ucwealth.loginregistrationsystem.model.Role;
+import com.ucwealth.loginregistrationsystem.model.User;
+import com.ucwealth.loginregistrationsystem.repository.UserRepository;
+import com.ucwealth.loginregistrationsystem.web.dto.UserRegistrationDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private UserRepository userRepository;
+	
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+
+
+	@Override
+	public User save(UserRegistrationDto registrationDto) {
+		
+		User user = new User(registrationDto.getFirstName(), 
+				registrationDto.getLastName(), registrationDto.getEmail(), 
+				registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+		return userRepository.save(user);
+	}
+
+}
