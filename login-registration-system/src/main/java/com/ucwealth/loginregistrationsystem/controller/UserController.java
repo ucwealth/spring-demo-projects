c1e300c675d092751d9626e66e3875bcfc5b3537
ucwealth <ucheokeke16@gmail.com>
2022-12-28 15:12:20 +0100

    create reg template and add bootstrap

--- login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/controller/UserController.java
+++ login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/controller/UserController.java
@@ -1,5 +1,34 @@
 package com.ucwealth.loginregistrationsystem.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import com.ucwealth.loginregistrationsystem.service.UserService;
+import com.ucwealth.loginregistrationsystem.web.dto.UserRegistrationDto;
+
+@Controller
+@RequestMapping("/registration")
+public class UserController {
+	private UserService userService;
+
+	public UserController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@GetMapping
+	public String getRegistrationForm() {
+		return "registration";
+	}
+	
+	@PostMapping
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		userService.save(registrationDto);
+		return "redirect:/registration?success";
+	}
+	
 }
