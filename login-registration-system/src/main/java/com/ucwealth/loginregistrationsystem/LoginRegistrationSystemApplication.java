cffa3de4fc5a283e8f926cf4fa7a11111d723b18
ucwealth <ucheokeke16@gmail.com>
2022-12-28 10:34:12 +0100

    create login-system project

---
+++ login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/LoginRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ucwealth.loginregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ login-registration-system/src/test/java/com/ucwealth/loginregistrationsystem/LoginRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucwealth.loginregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
