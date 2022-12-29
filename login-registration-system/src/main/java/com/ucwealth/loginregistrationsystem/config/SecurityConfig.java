f4310b229d95c4531da8eef01b894968bccc9782
ucwealth <ucheokeke16@gmail.com>
2022-12-29 10:35:04 +0100

    setup auth manager builder

--- login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/config/SecurityConfig.java
+++ login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,16 +34,20 @@ public class SecurityConfig {
 		return auth;
 	}
 	
+	protected void configure(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         
         http
+        .csrf().disable()
+        .cors().and()
         .authorizeHttpRequests()
         .requestMatchers("/registration**", "/js/**", "/css/**", "/img/**")
-        .permitAll().anyRequest()
-        .authenticated()
-        .and().formLogin()
-        .loginPage("/login")
+        .permitAll().anyRequest().authenticated()
+        .and().formLogin().loginPage("/login")
         .permitAll()
         .and().logout()
         .invalidateHttpSession(true)
--- login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/repository/UserRepository.java
+++ login-registration-system/src/main/java/com/ucwealth/loginregistrationsystem/repository/UserRepository.java
@@ -7,5 +7,5 @@ import com.ucwealth.loginregistrationsystem.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	User findByEmail(String email);
 }
